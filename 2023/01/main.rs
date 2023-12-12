b3067d3030bb53e1faa8a0fb5d1ed3a4080dbcca
Jason Connell <jasontconnell@gmail.com>
2023-12-12 00:43:50 -0500

    Run through 2023 day 1 in rust

---
+++ 2023/01/main.rs
@@ -0,0 +1,82 @@
+use std::fs;
+
+fn main(){
+    let filename = match std::env::args().nth(1) {
+        Some(s) => s,
+        None => String::from("input.txt")
+    };
+    let contents = read_file(filename);
+    let lines: Vec<&str> = contents.lines().collect();
+    let p1: i32 = part1(lines.clone());
+    let p2: i32 = part2(lines.clone());
+
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
+}
+
+fn part1(lines: Vec<&str>) -> i32 {
+    let mut ret: i32 = 0;
+    for line in lines.iter() {
+        let mut nums = Vec::new();
+        for (_i, c) in line.chars().enumerate() {
+            if c.is_numeric() {
+                let n = match c.to_string().parse::<i32>() {
+                    Ok(n) => n,
+                    Err(_e) => 0
+                };
+                nums.push(n);
+            }
+        }
+        let f: i32 = match nums.get(0) {
+            Some(n) => *n,
+            None => 0
+        };
+        let lst: i32 = match nums.last() {
+            Some(n) => *n,
+            None => 0
+        };
+        ret += (f*10) + lst;
+    }
+    return ret;
+}
+
+fn part2(lines: Vec<&str>) -> i32 {
+    let numlookup: [&str; 10] = ["zero","one","two","three","four","five","six","seven","eight","nine"];
+    let mut ret: i32 = 0;
+    for line in lines.iter() {
+        let mut nums = Vec::new();
+        for (i, c) in line.chars().enumerate() {
+            if c.is_numeric() {
+                let n = match c.to_string().parse::<i32>() {
+                    Ok(n) => n,
+                    Err(_e) => 0
+                };
+                nums.push(n);
+            } else {
+                for x in 1..10 {
+                    let s = numlookup[x];
+                    if i + s.len() <= line.len() && line[i..i+s.len()] == *s {
+                        nums.push(x as i32);
+                        break;
+                    }
+                }
+            }
+        }
+        let f: i32 = match nums.get(0) {
+            Some(n) => *n,
+            None => 0
+        };
+        let lst: i32 = match nums.last() {
+            Some(n) => *n,
+            None => 0
+        };
+        ret += (f*10) + lst;
+    }
+
+    return ret;
+}
+
+fn read_file(filename: String) -> String {
+    let contents = fs::read_to_string(filename).unwrap();
+    return contents
+}
